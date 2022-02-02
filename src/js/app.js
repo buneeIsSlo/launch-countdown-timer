21095649058fd8fdb846e33413da346235121848
bunee <buneeisslo@gmail.com>
2022-02-02 17:51:09 +0530

    Test: create static particles

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,42 @@
 const canvas = document.getElementById("canvas");
-const drawingContext = canvas.getContext("2d");
+const canvasContext = canvas.getContext("2d");
 
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-
-const testDraw = () => {
-    //drawingContext.clearRect(0, 0, canvas.width, canvas.height);
-    drawingContext.beginPath();
-    drawingContext.arc(x, y, 25, 0, 2 * Math.PI, false);
-    drawingContext.fill();
-
-    //x += 1;
-    //y += 1;
-
-    //window.requestAnimationFrame(testDraw);
+const particleCollection = [];
+const particleSize = {min: 1, max: 3};
+const particleVelocity = {
+    velocityX: {min: .03, max: .05},
+    velocityY: {min: .03, max: .05}
 }
 
-testDraw();
-console.log("iss a tes!");
+window.addEventListener("load", () => {
+    setUpCanvas();
+    particlesVolume();
+    particleCollection.forEach(particleInfo => drawParticle(particleInfo));
+})
+
+const setUpCanvas = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    canvasContext.fillStyle = "#fff";
+}
+
+const particlesVolume = () => {
+    for(let i = 0; i < 50; i++) {
+        particleCollection.push(createParticle());
+    }
+}
+
+const createParticle = (particle = {}, prevPosY = null) => {
+    particle.randomPosX = Math.random() * canvas.width;
+    particle.randomPosY = prevPosY ? prevPosY : Math.random() * canvas.height;
+    particle.randomSize = (Math.random() * (particleSize.max - particleSize.min)) + particleSize.min;
+
+    return particle;
+}
+
+const drawParticle = (particleObj) => {
+    canvasContext.beginPath();
+    canvasContext.arc(particleObj.randomPosX, particleObj.randomPosY, particleObj.randomSize, 0, 2 * Math.PI);
+    canvasContext.fill();
+}
